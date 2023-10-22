f533ecb1895487d2282f8fc739638b2d16650dac
khrabanm <khrabanm@yandex.ru>
2023-10-22 15:06:44 +0300

    refactor: remove divider

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,13 +1,12 @@
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
-import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 function Movies({ movies, isSaved = false }) {
   return (
     <section className="movies">
       <div className="movies__container container">
-        <SearchForm />
-        <div className="movies__divider" />
+        <SearchForm className="movies__divider" />
         <MoviesCardList movies={movies} isSaved={isSaved} />
       </div>
     </section>
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useState } from 'react';
 import './SearchForm.css';
 import search from '../../images/search.svg';
 import Button from '../Button/Button';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
-function SearchForm() {
+function SearchForm({ className }) {
   const [checked, setChecked] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <section className="search-form">
+    <section className={`search-form ${className || ''}`}>
       <form className="search-form__form" onSubmit={handleSubmit}>
         <div className="search-form__container">
           <input className="search-form__input" placeholder="Фильм" required />
