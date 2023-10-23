62433305610b8d8b1fdccc80c73718e0f622b132
khrabanm <khrabanm@yandex.ru>
2023-10-24 02:30:18 +0300

    refactor: change prop fields

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -3,29 +3,40 @@ import './MoviesCard.css';
 import Button from '../Button/Button';
 import deleteSaved from '../../images/delete-saved.svg';
 import RadioButton from '../RadioButton/RadioButton';
+import { MOVIES_IMAGE_URL } from '../../utils/consts';
+
+const getDuration = (duration) => {
+  const hours = Math.trunc(duration / 60);
+  const minutes = duration % 60;
+  return `${hours ? `${hours}ч` : ''} ${minutes}м`;
+};
 
 function MoviesCard({ movie, isSaved }) {
   const [checked, setChecked] = useState(false);
 
   return (
     <article className="movies-card">
-      <img className="movies-card__image" src={movie.image} alt={movie.name} />
+      <img
+        className="movies-card__image"
+        src={`${MOVIES_IMAGE_URL}${movie.image.url}`}
+        alt={movie.nameRU}
+      />
       <div className="movies-card__info">
         <div className="movies-card__title-container">
-          <h2 className="movies-card__title">{movie.name}</h2>
+          <h2 className="movies-card__title">{movie.nameRU}</h2>
           {isSaved ? (
             <Button className="movies-card__delete" type="button">
               <img src={deleteSaved} alt="Удалить" />
             </Button>
           ) : (
             <RadioButton
-              name={movie.name}
+              name={movie.nameRU}
               checked={checked}
               onChange={() => setChecked((prev) => !prev)}
             />
           )}
         </div>
-        <p className="movies-card__duration">{movie.duration}</p>
+        <p className="movies-card__duration">{getDuration(movie.duration)}</p>
       </div>
     </article>
   );
