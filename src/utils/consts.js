0c9f71fa3f7b39f6bb86d6be1f386a0107a0544e
khrabanm <khrabanm@yandex.ru>
2023-10-24 02:28:43 +0300

    feat: add consts.js

---
+++ src/utils/consts.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/prefer-default-export
+export const MOVIES_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+export const MOVIES_IMAGE_URL = 'https://api.nomoreparties.co/';
+
+export const SHORT_MOVIE_DURATION = 40;
+
+export const MOVIES_COUNT = {
+  DESKTOP: 16,
+  TABLET: 8,
+  MOBILE: 5,
+  DESKTOP_ADD: 4,
+  TABLET_ADD: 2,
+  MOBILE_ADD: 2,
+};
+
+export const MOVIES_TEXT_ERROR =
+  'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз';
+export const MOVIES_TEXT_NOT_FOUND = 'Ничего не найдено';
