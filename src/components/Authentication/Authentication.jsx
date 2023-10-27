4627e2352e957fd42a680a371b44c75944dbf38f
khrabanm <khrabanm@yandex.ru>
2023-10-28 01:53:55 +0300

    feat: add logic for auth

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Authentication.css';
 import Logo from '../Logo/Logo';
 import Button from '../Button/Button';
 import Link from '../Link/Link';
+import {
+  EMAIL_MAX,
+  EMAIL_MIN,
+  NAME_MAX,
+  NAME_MIN,
+  PASSWORD_MAX,
+  PASSWORD_MIN,
+  SERVER_ERRORS_CODE,
+  SIGNUP_ERROR_MESSAGES,
+  SIGNIN_ERROR_MESSAGES,
+} from '../../utils/consts';
+import useFormWithValidation from '../../hooks/useFormWithValidation';
+import Preloader from '../Preloader/Preloader';
 
-function Authentication({ isSignIn = true }) {
-  const [error, setError] = useState('Что-то пошло не так...');
-  console.log(setError);
-  const onSubmit = (e) => {
+function Authentication({ isSignIn = true, onSubmit }) {
+  const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation(isSignIn);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = (e) => {
+    if (isLoading) return;
+    setIsLoading(true);
     e.preventDefault();
+    onSubmit(values)
+      .catch((err) => {
+        const errorText = isSignIn
+          ? SIGNIN_ERROR_MESSAGES[SERVER_ERRORS_CODE[err.message]]
+          : SIGNUP_ERROR_MESSAGES[SERVER_ERRORS_CODE[err.message]];
+        setError(errorText || err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
+
+  useEffect(() => {
+    resetForm();
+    setError('');
+  }, [isSignIn, resetForm]);
 
   return (
     <main className="authentication">
@@ -17,67 +48,93 @@ function Authentication({ isSignIn = true }) {
         <Logo />
         <h1 className="authentication__title">{isSignIn ? 'Рады видеть!' : 'Добро пожаловать!'}</h1>
       </header>
-      <form onSubmit={onSubmit} className="authentication__form" id="authentication">
-        <div className="authentication__form-wrapper">
-          {!isSignIn && (
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <form onSubmit={handleSubmit} className="authentication__form" id="authentication">
+          <div className="authentication__form-wrapper">
+            {!isSignIn && (
+              <div className="authentication__item">
+                <label className="authentication__label" htmlFor="name">
+                  Имя
+                </label>
+                <input
+                  placeholder="Имя"
+                  minLength={NAME_MIN}
+                  maxLength={NAME_MAX}
+                  className={`authentication__input ${
+                    errors.name && 'authentication__input_error'
+                  }`}
+                  name="name"
+                  id="name"
+                  type="text"
+                  required
+                  value={values.name || ''}
+                  onChange={handleChange}
+                />
+                {errors.name && <span className="authentication__error">{errors.name}</span>}
+              </div>
+            )}
             <div className="authentication__item">
-              <label className="authentication__label" htmlFor="name">
-                Имя
+              <label className="authentication__label" htmlFor="email">
+                E-mail
               </label>
               <input
-                placeholder="Имя"
-                minLength="2"
-                maxLength="30"
-                className="authentication__input"
-                name="name"
-                id="name"
-                type="text"
+                placeholder="E-mail"
+                className={`authentication__input ${errors.email && 'authentication__input_error'}`}
+                minLength={EMAIL_MIN}
+                maxLength={EMAIL_MAX}
+                name="email"
+                id="email"
+                type="email"
                 required
+                value={values.email || ''}
+                onChange={handleChange}
               />
+              {errors.email && <span className="authentication__error">{errors.email}</span>}
             </div>
-          )}
-          <div className="authentication__item">
-            <label className="authentication__label" htmlFor="email">
-              E-mail
-            </label>
-            <input
-              placeholder="E-mail"
-              className="authentication__input"
-              name="email"
-              id="email"
-              type="email"
-              required
-            />
+            <div className="authentication__item">
+              <label className="authentication__label" htmlFor="password">
+                Пароль
+              </label>
+              <input
+                placeholder="Пароль"
+                minLength={isSignIn ? NAME_MIN : PASSWORD_MIN}
+                maxLength={isSignIn ? EMAIL_MAX : PASSWORD_MAX}
+                className={`authentication__input ${
+                  errors.password && 'authentication__input_error'
+                }`}
+                name="password"
+                id="password"
+                type="password"
+                required
+                value={values.password || ''}
+                onChange={handleChange}
+              />
+              {errors.password && <span className="authentication__error">{errors.password}</span>}
+            </div>
           </div>
-          <div className="authentication__item">
-            <label className="authentication__label" htmlFor="password">
-              Пароль
-            </label>
-            <input
-              placeholder="Пароль"
-              minLength="8"
-              maxLength="30"
-              className={`authentication__input ${error && 'authentication__input_error'}`}
-              name="password"
-              id="password"
-              type="password"
-              required
-            />
+          <footer className="authentication__footer">
             {error && <span className="authentication__error">{error}</span>}
-          </div>
-        </div>
-        <footer className="authentication__footer">
-          <Button type="submit" form="authentication" className="authentication__button">
-            {isSignIn ? 'Войти' : 'Зарегистрироваться'}
-          </Button>
-          <p className="authentication__text">
-            {isSignIn ? 'Ещё не зарегистрированы?' : 'Уже зарегистрированы?'}
-            <Link to={isSignIn ? '/signup' : '/signin'} className="authentication__link">
-              {isSignIn ? 'Регистрация' : 'Войти'}
-            </Link>
-          </p>
-        </footer>
-      </form>
+            <Button
+              type="submit"
+              form="authentication"
+              className={`authentication__button ${
+                !isValid ? 'authentication__button_disabled' : ''
+              }`}
+              disabled={!isValid || isLoading}
+            >
+              {isSignIn ? 'Войти' : 'Зарегистрироваться'}
+            </Button>
+            <p className="authentication__text">
+              {isSignIn ? 'Ещё не зарегистрированы?' : 'Уже зарегистрированы?'}
+              <Link to={isSignIn ? '/signup' : '/signin'} className="authentication__link">
+                {isSignIn ? 'Регистрация' : 'Войти'}
+              </Link>
+            </p>
+          </footer>
+        </form>
+      )}
     </main>
   );
 }
