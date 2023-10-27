524d3c06f88f19929fb2148d07ddfc637989f4e0
khrabanm <khrabanm@yandex.ru>
2023-10-28 01:53:31 +0300

    feat: make MoviesApi extended from Api

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,25 +1,12 @@
 import { MOVIES_API_URL } from './consts';
+import Api from './Api';
 
-class MoviesApi {
-  constructor(options) {
-    this.headers = {
-      ...options.headers,
-    };
-    this.url = options.baseUrl;
-  }
-
-  static handleResponse(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(new Error(`Error: ${res.status}`));
-  }
-
+class MoviesApi extends Api {
   getMovies() {
     return fetch(this.url).then((res) => MoviesApi.handleResponse(res));
   }
 }
 
 export default new MoviesApi({
-  baseUrl: MOVIES_API_URL,
+  url: MOVIES_API_URL,
 });
