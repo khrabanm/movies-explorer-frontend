b7ce4290b6c7e7902d59834f3dda5a9f7ae1c6ab
khrabanm <khrabanm@yandex.ru>
2023-10-28 01:51:44 +0300

    feat: add new consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const MOVIES_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+export const MAIN_API_URL = 'https://api.movies-explorer.khr.nomoredomainsrocks.ru';
 export const MOVIES_IMAGE_URL = 'https://api.nomoreparties.co/';
 
 export const SHORT_MOVIE_DURATION = 40;
@@ -16,3 +17,42 @@ export const MOVIES_COUNT = {
 export const MOVIES_TEXT_ERROR =
   'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз';
 export const MOVIES_TEXT_NOT_FOUND = 'Ничего не найдено';
+
+export const FORM_REGEXP = {
+  name: /^[a-zA-ZА-Яа-яЁё\s].{1,19}$/,
+  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
+};
+
+export const FORM_ERROR_MESSAGES = {
+  name: 'Имя должно быть от 2 до 20 символов',
+  email: 'Неправильный формат email',
+  password: 'Пароль должен быть от 8 до 30 символов, содержать цифры, заглавные и строчные буквы',
+};
+
+export const SERVER_ERRORS_CODE = {
+  'Error: 400': 400,
+  'Error: 401': 401,
+  'Error: 409': 409,
+};
+
+export const SIGNUP_ERROR_MESSAGES = {
+  400: 'Некорректно заполнено одно из полей',
+  409: 'При регистрации пользователя произошла ошибка',
+};
+
+export const SIGNIN_ERROR_MESSAGES = {
+  401: 'Вы ввели неправильный логин или пароль.',
+  400: 'Вы ввели неправильный логин или пароль.',
+};
+
+export const PROFILE_ERRORS_MESSAGES = {
+  400: 'При обновлении профиля произошла ошибка.',
+  409: 'Пользователь с таким email уже существует.',
+};
+
+export const NAME_MIN = 2;
+export const NAME_MAX = 20;
+export const EMAIL_MIN = 8;
+export const EMAIL_MAX = 200;
+export const PASSWORD_MIN = 8;
+export const PASSWORD_MAX = 30;
