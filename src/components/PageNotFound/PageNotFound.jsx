dbffda996851c29683ba76c4b85159dac3370566
khrabanm <khrabanm@yandex.ru>
2023-10-30 17:43:17 +0300

    refactor: change link to back

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -8,7 +8,7 @@ function PageNotFound() {
         <h1 className="page-not-found__title">404</h1>
         <p className="page-not-found__text">Страница не найдена</p>
       </div>
-      <Link to="/" className="page-not-found__link">
+      <Link to={-1} className="page-not-found__link">
         Назад
       </Link>
     </main>
