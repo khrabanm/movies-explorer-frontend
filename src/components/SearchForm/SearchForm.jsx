988880f25e72562dd4298a4ee0f0d92751ae691d
khrabanm <khrabanm@yandex.ru>
2023-10-30 14:13:36 +0300

    feat: add search form validation

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,28 +1,41 @@
+import { useState } from 'react';
 import './SearchForm.css';
 import search from '../../images/search.svg';
 import Button from '../Button/Button';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+import { FORM_ERROR_MESSAGES } from '../../utils/consts';
 
 function SearchForm({ className, query, isShortMovies, onChangeQuery, onSearch, onFilter }) {
+  const [isEmpty, setIsEmpty] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!query.trim()) {
+      setIsEmpty(true);
+      return;
+    }
     onSearch(query, isShortMovies);
+  };
+
+  const handleChangeQuery = (e) => {
+    onChangeQuery(e);
+    setIsEmpty(false);
   };
 
   return (
     <section className={`search-form ${className || ''}`}>
       <form className="search-form__form" onSubmit={handleSubmit}>
-        <div className="search-form__container">
+        <div className={`search-form__container ${isEmpty ? 'search-form__container--error' : ''}`}>
           <input
             className="search-form__input"
             value={query}
-            onChange={onChangeQuery}
+            onChange={handleChangeQuery}
+            name="search"
             placeholder="Фильм"
-            required
           />
           <Button className="search-form__button" type="submit">
             <img src={search} alt="Поиск" />
           </Button>
+          {isEmpty && <span className="search-form__error">{FORM_ERROR_MESSAGES.search}</span>}
         </div>
         <FilterCheckbox text="Короткометражки" checked={isShortMovies} onChange={onFilter} />
       </form>
