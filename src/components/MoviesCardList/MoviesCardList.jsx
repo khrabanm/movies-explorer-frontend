b95ba965a029626730106b9567c4cf3402283705
khrabanm <khrabanm@yandex.ru>
2023-10-30 23:01:33 +0300

    refactor: remove visible movies for saved movies

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -4,10 +4,8 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 import Button from '../Button/Button';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
 import { BREAKPOINTS, MOVIES_COUNT } from '../../utils/consts';
-import usePrevious from '../../hooks/usePrevious';
 
 const getVisibleMovies = (width) => {
-  console.log('getVisibleMovies', width);
   if (width <= BREAKPOINTS.MOBILE_MAX) return MOVIES_COUNT.MOBILE;
   if (width <= BREAKPOINTS.TABLET_MAX) return MOVIES_COUNT.TABLET;
   if (width <= BREAKPOINTS.LAPTOP_MAX) return MOVIES_COUNT.LAPTOP;
@@ -39,7 +37,7 @@ function MoviesCardList({ movies, savedMovies, onSave, onDelete, isSaved = false
     <section className="movies-card-list">
       <ul className="movies-card-list__list">
         {movies.map((movie, idx) => {
-          if (idx >= visibleMovies) return null;
+          if (idx >= visibleMovies && !isSaved) return null;
           return (
             <li className="movies-card-list__item" key={movie.movieId}>
               <MoviesCard
@@ -53,7 +51,7 @@ function MoviesCardList({ movies, savedMovies, onSave, onDelete, isSaved = false
           );
         })}
       </ul>
-      {movies.length > visibleMovies && (
+      {movies.length > visibleMovies && !isSaved && (
         <Button className="movies-card-list__button" type="button" onClick={handleAddMovies}>
           Ещё
         </Button>
